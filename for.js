b6cfd488bd5b632fb9c347b7e30f9f6477d2fd6c
aoamy <2465602445@qq.com>
2017-05-04 14:43:47 +0800

    Initial commit

---
+++ for.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+
+
+module.exports = function( a, b )
+{
+	let _s = 0;
+	for( let i = a; i <= b; i++ )
+	{
+		_s += i;
+	}
+	return _s;
+};
+// console.log(add(2,10));
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+"use strict";
+const sum=require("./for");
+module.exports=sum;
+// console.log(sum);
+// console.log(sum(1,100));
